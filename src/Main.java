a4d12e3a2465ab307362099f7e1b912ca24667d8
Greg Poslov <grig.skamsk@mail.ru>
2022-09-19 16:28:12 +0500

    Changed names

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         Scanner tp = new Scanner(System.in);
         System.out.println("Сумма пополнения счета:");
 
-        int ticket_price = tp.nextInt(); //Стоимость биллета
+        int ticketPrice = tp.nextInt(); //Стоимость биллета
 
-        if (ticket_price < 1000) {
+        if (ticketPrice < 1000) {
             System.out.println("Количество бонусов = 0");
         } else {
-            System.out.println("Количество бонусов = " + ticket_price / 100);
+            System.out.println("Количество бонусов = " + ticketPrice / 100);
         }
 
 
